a39bb081cad198627e01b2659a29dcf21c75564b
Hafeez <feeziman007@hotmail.com>
2017-12-11 02:41:32 +1100

    node proxy file added

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+const path = require('path');
+const express = require('express');
+const proxy = require('express-http-proxy');
+const baseImageUrl = process.env.BASE_IMAGE_URL;
+const proxyBaseImageUrl = baseImageUrl
+  ? proxy(baseImageUrl, {
+      proxyReqPathResolver: function (req) {
+        const newPath = baseImageUrl + req.path;
+        console.log(newPath);
+        return newPath;
+      }
+    })
+  : express.static(path.join(__dirname, 'public/images'));
+const app = express();
+
+app.use('/images', proxyBaseImageUrl);
+
+app.listen(8880);
+
+
+
+
+
+
+
+
+
+
 require('dotenv').config();
+
 var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect(process.env.MONGO_URI, function(err, db) {
