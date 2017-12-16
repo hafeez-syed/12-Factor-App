a1884df26cc96ffa2a874cf96ef3ea46cba85dd4
Hafeez <feeziman007@hotmail.com>
2017-12-17 03:54:57 +1100

    index updated

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 const path = require('path');
 const express = require('express');
 const proxy = require('express-http-proxy');
+const morgan = require('morgan');
 const baseImageUrl = process.env.BASE_IMAGE_URL;
 const proxyBaseImageUrl = baseImageUrl
   ? proxy(baseImageUrl, {
       proxyReqPathResolver: function (req) {
         const newPath = baseImageUrl + req.path;
-        console.log(newPath);
+        console.log(`Proxying requests from ${req.path} to ${newPath}`);
         return newPath;
       }
     })
   : express.static(path.join(__dirname, 'public/images'));
-const app = express();
 
+  const app = express();
+
+app.use(morgan('combined'));
 app.use('/images', proxyBaseImageUrl);
 
-app.listen(8880);
+app.get('/', function (req, res) {
+  res.send('<h1>Hello from Herman</h1><img src="images/herman.jpg" />')
+})
 
-
-
-
-
-
-
-
-
+app.listen(8080, () => console.log('Web server running on port 8880'));
 
 /*
 require('dotenv').config();
