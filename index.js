eafe728221a96f64529ba9d285d1a1c63422d0c7
Hafeez <feeziman007@hotmail.com>
2017-12-17 03:01:06 +1100

    env deleted.

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const path = require('path');
 const express = require('express');
 const proxy = require('express-http-proxy');
+const morgan = require('morgan');
 const baseImageUrl = process.env.BASE_IMAGE_URL;
 const proxyBaseImageUrl = baseImageUrl
   ? proxy(baseImageUrl, {
       proxyReqPathResolver: function (req) {
         const newPath = baseImageUrl + req.path;
-        console.log(newPath);
+        console.log(`Proxying requests from ${req.path} to ${newPath}`);
         return newPath;
       }
     })
   : express.static(path.join(__dirname, 'public/images'));
+
 const app = express();
 
+app.use(morgan('combined'));
 app.use('/images', proxyBaseImageUrl);
 
 app.listen(8880);
